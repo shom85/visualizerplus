1dd9e1f0a99df327c69f057041b45c44d8c2aea7
Shomit <shomit@marble.com>
2017-06-28 16:37:08 -0400

    testing of the chair for housetipster

--- js/app.js
+++ js/app.js
@@ -49,6 +49,21 @@ function init(){
         disableSpin: true // Default false
     });
 
+    product4 = $('.chair').ThreeSixty({
+        totalFrames: 74, // Total no. of image you have for 360 slider
+        endFrame: 74, // end frame for the auto spin animation
+        currentFrame: 1, // This the start frame for auto spin
+        imgList: '.threesixty_images', // selector for image list
+        progress: '.spinner', // selector to show the loading progress
+        imagePath:'assets/chair/', // path of the image assets
+        ext: '.jpg', // extention for the assets
+        height: 265,
+        width: 400,
+        navigation: true,
+        responsive: true,
+        disableSpin: true // Default false
+    });
+
 }
 
 $(function() {
